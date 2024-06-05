d6efccfc224ba97baea816c345ad8bf49b88a197
YannosK <giannos2405@gmail.com>
2024-06-05 17:39:07 +0300

    it seems correct but VIVADO crushes

--- UART.srcs/sources_1/new/UART_receiver.vhd
+++ UART.srcs/sources_1/new/UART_receiver.vhd
@@ -55,7 +55,10 @@ architecture Behavioral of UART_receiver is
     signal current_state    : FSM_states := RX_idle;
     signal next_state       : FSM_states;
 
+    -- signal start_RX     : std_logic := '1';             -- 
+    signal start_baud   : std_logic := '0';             -- signal to start the baud16 counter
     signal baud_count   : integer := 0;                 -- mod 16 counter to define sample moment
+    signal baud_tick    : std_logic := '0';             -- becomes '1' if the counter counted to 16
     signal fill_SIPO    : std_logic := '0';             -- signal to start filling SIPO from RxD. Connects to 'start' of SIPO
     signal filled_SIPO  : std_logic;                    -- signal that SIPO filled up
     signal data_internal: std_logic_vector(7 downto 0); -- data passed around from SIPO to FIFO
@@ -64,6 +67,10 @@ architecture Behavioral of UART_receiver is
     signal empty_FIFO   : std_logic;                    -- If '1' FIFO is completely empty (no read allowed). Connects to 'empty' in FIFO
 
     begin
+        
+        -----------------------------------------------------------------------------------------------------------------
+        -- modules
+        -----------------------------------------------------------------------------------------------------------------
 
         RX_FIFO: fifo_generator_0 port map
                 (
@@ -86,8 +93,18 @@ architecture Behavioral of UART_receiver is
                     s_in     => RxD,
                     p_out    => data_internal
                 );
+        
+        -----------------------------------------------------------------------------------------------------------------
+        -- signals
+        -----------------------------------------------------------------------------------------------------------------
 
         RX_Valid <= not empty_FIFO;
+
+        -----------------------------------------------------------------------------------------------------------------
+        -- processes
+        -----------------------------------------------------------------------------------------------------------------
+
+        -- baud_counter: process(baud_ref)
 
         state_reg: process (clock) is
             begin
@@ -109,7 +126,7 @@ architecture Behavioral of UART_receiver is
                         fill_SIPO <= '0';
                         write_FIFO <= '0';
                         baud_count <= 0;
-                        if falling_edge(RxD) then
+                        if RxD = '0' and RxD'event then
                             temp_state := RX_start_check;
                         end if;
                         if temp_state = RX_start_check then
@@ -118,21 +135,21 @@ architecture Behavioral of UART_receiver is
                             next_state <= RX_idle;
                         end if;
                     when RX_start_check =>
-                        if rising_edge(baud_ref) then
+                        temp_state := RX_start_check;
+                        if baud_ref = '1' and baud_ref'event then
                             if baud_count = 15 then         -- ATTENTION: not sure if it must be 15 or 16
                                 baud_count <= 0;
                                 if RxD = '0' then
-                                    next_state <= RX_data_fetch;
+                                    temp_state := RX_data_fetch;
                                 else
-                                    next_state <= RX_idle;
+                                    temp_state := RX_idle;
                                 end if;
                             else
                                 baud_count <= baud_count + 1;
-                                next_state <= RX_start_check;
+                                temp_state := RX_start_check;
                             end if;
-                        else                                -- ATTENTION: not sure if correct
-                            next_state <= RX_start_check;
                         end if;
+                        next_state <= temp_state;           -- ATTENTION: not sure if you can pass a variable to a signal
                     when RX_data_fetch  =>
                         fill_SIPO <= '1';
                         if filled_SIPO = '1' then
@@ -141,21 +158,21 @@ architecture Behavioral of UART_receiver is
                             next_state <= RX_data_fetch;
                         end if;
                     when RX_stop_check  =>
-                        if rising_edge(baud_ref) then
+                        temp_state := RX_stop_check;
+                        if baud_ref = '1' and baud_ref'event then
                             if baud_count = 15 then         -- ATTENTION: not sure if it must be 15 or 16
                                 baud_count <= 0;
                                 if RxD = '1' then
-                                    next_state <= RX_FIFO_write;
+                                    temp_state := RX_FIFO_write;
                                 else
-                                    next_state <= RX_idle;
+                                    temp_state := RX_idle;
                                 end if;
                             else
                                 baud_count <= baud_count + 1;
-                                next_state <= RX_stop_check;
+                                temp_state := RX_stop_check;
                             end if;
-                        else                                -- ATTENTION: not sure if correct
-                            next_state <= RX_stop_check;
                         end if;
+                        next_state <= temp_state;
                     when RX_FIFO_write  =>
                         if full_FIFO = '1' then
                             next_state <= RX_idle;
